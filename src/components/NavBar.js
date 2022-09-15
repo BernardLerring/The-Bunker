c36a4ded25661e3187d983c35c59b218b3d64875
Tim <orcatim@hotmail.com>
2022-09-15 16:47:02 +0000

    add and style nav links and routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import { Navbar, Container, Nav} from 'react-bootstrap';
 import thebunker from "../assets/thebunker.png";
+import styles from '../styles/NavBar.module.css';
+import { NavLink } from 'react-router-dom';
 
 
 const NavBar = () => {
-  return <Navbar expand="md" fixed="top">
+  return <Navbar className= {styles.NavBar} expand="md" fixed="top">
     <Container>
+      <NavLink to="/">
     <Navbar.Brand>
-      <img src = {thebunker} alt="site logo" height = "90"/></Navbar.Brand>
+      <img src = {thebunker} alt="site logo" height = "120"/></Navbar.Brand>
+      </NavLink>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="ml-auto text-left">
-      <Nav.Link>
+      <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
         <i className="fas fa-golf-ball"></i>Home
-        </Nav.Link>
-      <Nav.Link>
+        </NavLink>
+      <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
         <i className ="fas fa-door-open"></i>Sign In
-      </Nav.Link>
-      <Nav.Link>
-        <i className="fas fa-pen"></i>Sign Up
-      </Nav.Link>
+      </NavLink>
+      <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
+        <i className="fas fa-user-plus"></i>Sign Up
+      </NavLink>
     </Nav>
   </Navbar.Collapse>
   </Container>
