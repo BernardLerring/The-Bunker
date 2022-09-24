cebfce241142d8431f979cc30bbbde63784c27c9
Tim <orcatim@hotmail.com>
2022-09-24 10:58:29 +0000

    Add edit and delte functions to posts and display profile details

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -8,25 +8,29 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import Comment from "../comments/Comment";
 
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import Comment from "../comments/Comment";
+
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
 
-const currentUser = useCurrentUser();
-const profile_image = currentUser?.profile_image;
-const [comments, setComments] = useState({ results: [] });
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }, {data: comments}] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
-          axiosReq.get(`/comments/?post${id}`)
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
         setComments(comments);
@@ -44,26 +48,37 @@ const [comments, setComments] = useState({ results: [] });
         <p>Popular profiles for mobile</p>
         <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>
-        {currentUser ? (
-  <CommentCreateForm
-  profile_id={currentUser.profile_id}
-  profileImage={profile_image}
-  post={id}
-  setPost={setPost}
-  setComments={setComments}
-/>
-) : comments.results.length ? (
-  "Comments"
-) : null}
-{comments.results.length ? (
-  comments.results.map(comment => (
-    <Comment key={comment.id} {...comment} />
-  ))
-) : currentUser ? (
-  <span>No comments to display, be the first to write one!</span>
-) : (
-  <span>No comments yet.</span>
-)}
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+          {comments.results.length ? (
+            <InfiniteScroll
+              children={comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setPost={setPost}
+                  setComments={setComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            />
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments... yet</span>
+          )}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
